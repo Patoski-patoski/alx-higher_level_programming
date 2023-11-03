21f05ae575bb93142d0c954601bba01009ac52cc
patrick okafor <codesbypatrick@gmail.com>
2023-11-02 17:08:44 -0700

    a program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,6 @@
 
 if __name__ == "__main__":
     import hidden_4
-
-    names = dir(hidden_4)
-    filtered = [name for name in names if not name.startswith("__")]
-    for name in sorted(filtered):
-        print(name)
+    for names in dir(hidden_4):
+        if names[:2] != "__":
+            print(names)
