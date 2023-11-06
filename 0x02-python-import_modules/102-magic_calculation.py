71e6003a8722f73bc16b3bb11bd72f7e952252d1
patrick okafor <codesbypatrick@gmail.com>
2023-11-05 16:21:04 -0800

    bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,10 @@
 def magic_calculation(a, b):
-    add = __import__('magic_calculation_102').add
-    sub = __import__('magic_calculation_102').sub
+    add = magic_calculation_102.add
+    sub = magic_calculation_102.sub
 
     if a < b:
         c = add(a, b)
-        for i in range(4, 7):
+        for i in range(4, 6):
             c = add(c, i)
             return c
         else:
