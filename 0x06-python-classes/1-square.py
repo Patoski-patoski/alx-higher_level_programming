294a825c11ede9752d1827ba7fa3e18222097082
patrick okafor <patrickpeters911@gmail.com>
2023-11-21 16:58:06 -0800

    a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''Write a class Square that defines a square by: (based on 0-square.py)'''
+
+class Square:
+
+    ''' Square- the class to define a square'''
+    def __init__(self, size):
+        self.__size = size
