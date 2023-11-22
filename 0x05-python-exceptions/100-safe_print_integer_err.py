30463065f4fcb9bda4d511237f14b1326bd8ad6d
patrick okafor <patrickpeters911@gmail.com>
2023-11-22 08:40:07 -0800

    a function that prints an integer.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -10,3 +10,5 @@ def safe_print_integer_err(value):
     except (TypeError, NameError, ValueError) as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return False
+    else:
+        return False
