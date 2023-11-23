783eb03697e9ac4edb830f9d7568c30dae044b39
patrick okafor <patrickpeters911@gmail.com>
2023-11-23 09:14:51 -0800

    add: a method:that returns the current square area ; git push

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 
-''' a class Square that defines a square by: (based on 1-square.py) '''
+""" a class Square that defines a square by: (based on 1-square.py)"""
 
 
 class Square:
 
-    ''' Square- the class that defines a square'''
+    """Square- the class that defines a square"""
     def __init__(self, size=0):
+        """__init__"- (method) initialize attributes(arguments) of an object
+
+        Args:
+                self: a reference to an instance of the class
+                size: size of the square
+
+        Return:
+                None
+         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -15,4 +24,6 @@ class Square:
             self.__size = size
 
     def area(self):
+        """area: that returns the current square area"""
+
         return self.__size * self.__size
