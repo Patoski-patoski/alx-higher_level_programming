7eff47f81aec7933ab0008657e3fd3c4efbc2383
patrick okafor <patrickpeters911@gmail.com>
2023-11-27 20:31:49 -0800

    add 1:  a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,48 +3,17 @@
 
 class Rectangle:
 
-    """ A class representing a rectangle.
-
-    Args:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-
-    Attributes:
-        __width (int): The width of the rectangle
-        __height (int): The height of the rectangle.
-    """
 
     def __init__(self, width=0, height=0):
-
-        """ Initializer for the Rectangle class.
-
-        Args:
-            width (int): The width of the rectangle (default: 0).
-            height (int): The heigth of the rectangle (default: 0).
-        """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """Get the width of the rectangle
-
-        Returns:
-            int: The width of the rectangle
-        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle
-
-        Args:
-            value (int): The new value of the width
-
-        Raises:
-            ValueError: If the width is less than 0
-            TypeError: If the width is not an integer
-        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -54,24 +23,11 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get the height of the rectangle
-
-        Returns:
-            int: the height of the triangle
-        """
         return self.__height
 
     @height.setter
     def height(self, value):
 
-        """Set the height of the rectangle
-         Args:
-            value (int): the new value of the rectangle
-
-         Raises:
-            TypeError: If the height is not an integer
-            ValueError: If the height is less than 0
-         """
         if not isinstance(value, int):
             raise TypeError(" height must be an integer")
         elif value < 0:
