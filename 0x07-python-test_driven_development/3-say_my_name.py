79a5c8160e674b7ae35ecf17b58783f9702e2d7e
patrick okafor <patrickpeters911@gmail.com>
2023-11-29 11:18:22 -0800

    a function to say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""function that prints My name is <first name> <last name>
 
-Test file for this module is located in ./tests/ directory
-Run (python3 -m doctest -v ./tests/3-say_my_name.txt | tail -2) to test.
-remove the pipeline to see full details"""
+"""This module demonstrates 3-say_my_name-
+   a function that prints My name is <first name> <last name>
+
+   Test file for this module is located in ./tests/ directory
+   Run (python3 -m doctest -v ./tests/2-matrix_divided.txt) to test
+
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """say_my_name - prints My name is <first name> <last name>
-    Args:
-        firstname: a string
-        last_name: a string
-        Return:
-            Nothing
-    """
+    """say_my_name- a function that prints first and last name
 
-    if not isinstance(first_name, str):
-        raise TypeError('first_name must be a string')
-    if not isinstance(last_name, str):
-        raise TypeError('last_name must be a string')
-    space = " "
-    print(f"My name is {first_name}{space}{last_name}")
+    Args:
+        first_name (str): first parameter
+        last_name (str): second parameter
+
+    Return:
+        Nothing
+    """
+    if type(first_name) not in [str]:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) not in [str]:
+        raise TypeError("last_name must be a string")
+    else:
+        print(f"My name is {first_name} {last_name}")
