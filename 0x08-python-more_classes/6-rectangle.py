089430915f7f33cd43df71623b6f9eb723765acf
patrick okafor <patrickpeters911@gmail.com>
2023-11-29 18:10:22 -0800

    add: a class Rectangle that defines a rectangle by: (based on 5-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -104,5 +104,5 @@ class Rectangle:
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
-        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
