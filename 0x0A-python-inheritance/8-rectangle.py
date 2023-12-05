c2ed66729bcc381ec77d857c4d4cf30dc38d192e
patrick okafor <patrickpeters911@gmail.com>
2023-12-05 07:38:22 -0800

    a class Rectangle that inherits from BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" The 7-base_geometry module"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ derived class Reactangle- a class Rectangle that inherits
+    from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        def __init__(self, width, height):
+            self.__width = width
+            self.__height = height
+            self.integer_validator("width", self.__width)
+            self.integer_validator("height", self.__height)
