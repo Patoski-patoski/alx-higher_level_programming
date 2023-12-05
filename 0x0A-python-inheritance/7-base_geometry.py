808cc0273d46001adeec2e83671db0417159d310
patrick okafor <patrickpeters911@gmail.com>
2023-12-05 06:41:30 -0800

    a function that validates value

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 
-""" The 5-base_geometry module"""
+""" The 7-base_geometry module"""
 
 
 class BaseGeometry:
 
-    """ class Geometry"""
+    """ class BaseGeometry"""
     def area(self):
         raise Exception("area() is not implemented")
 
 
     def integer_validator(self, name, value):
-        """validate the value input"""
+
+        """integer_validator- to validate an object for integer
+
+        Args:
+            name (str): firs parameter
+            value (int): Second parameter
+
+        Returns:
+            Nothing
+        """    
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
