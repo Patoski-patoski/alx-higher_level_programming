291587a88a39d5fe14b45e65ff0b691655d85ebf
patrick okafor <patrickpeters911@gmail.com>
2023-12-05 20:48:24 -0800

    a function that writes a string to a text file (UTF8)

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -13,6 +13,5 @@ def write_file(filename="", text=""):
     Return:
         the lenht of characters written
     """
-    def write_file(filename="", text=""):
-        with open(filename, "w", encoding="utf-8") as f:
-            return f.write(text)
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
