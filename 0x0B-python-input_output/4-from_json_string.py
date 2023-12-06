fcd58c81c5cb736fb2f4430d780a68cb0c04bdb4
patrick okafor <patrickpeters911@gmail.com>
2023-12-05 20:55:12 -0800

    a function that returns an object (Python data structure) represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""The 4-from_json_string module"""
+
+
+def from_json_string(my_str):
+
+    """to_json_string-def- to_json_string(my_obj)
+    Args:
+        obj (str): the object
+    Return:
+         returns the JSON string representation
+    """
+    return json.loads(my_obj)
