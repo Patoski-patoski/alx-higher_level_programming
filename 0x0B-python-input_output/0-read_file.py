9427c43d1e622408cd207f2f6cefcb796555dba2
patrick okafor <patrickpeters911@gmail.com>
2023-12-05 18:59:18 -0800

    a function that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,9 @@
 
 """The 0-read_file module"""
 
+
 def read_file(filename=""):
+
     """read_file-  reads a text file (UTF8) and prints it to stdout
     Args:
         filename (str): firs parameter
