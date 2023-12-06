236f533cabc1c4bc9d56add98b61858026c20c4f
patrick okafor <patrickpeters911@gmail.com>
2023-12-06 14:51:29 -0800

    fix bug: a function where the area method is implemented

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -17,9 +17,25 @@ class Rectangle(BaseGeometry):
         self.integer_validator("width", self.__width)
         self.integer_validator("height", self.__height)
 
-    def __str__(self):
-        return "[{}] {}/{}".format(self.__class__.__name__, self.__width, self.__height)
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
 
     def area(self):
-        return "{}".format(self.__height * self.__width)
+        return self.__height * self.__width
 
+    def __str__(self):
+        return f"[{Rectangle.__name__}] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        super()._init__(width=size, height=size)
+
+    def area(self):
+        return self.__width * self.__height
