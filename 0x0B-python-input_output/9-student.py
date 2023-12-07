0f83075ed486d2a6a7493cc27fb213a8a3462f47
patrick okafor <patrickpeters911@gmail.com>
2023-12-06 21:00:11 -0800

    Write a class Student that defines a student by that retrives a dic representation

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,12 +9,11 @@ class Student:
     """Student class """
 
     def __init__(self, first_name, last_name, age):
-    """initialize instances to class"""
+        """initialize instances to class"""
 
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
     def to_json(self):
-
-    """retrieves a dictionary representation of a Student instance"""
-    return vars(self)
+        return vars(self)
