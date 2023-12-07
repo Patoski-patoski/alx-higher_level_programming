f00682b2f410417ea9d0890143aec0de5a8364f1
patrick okafor <patrickpeters911@gmail.com>
2023-12-06 20:36:07 -0800

    a function that creates an Object from a “JSON file”:

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+6-load_from_json_file module
+load_from_json_file function
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """ creates an Object from a “JSON file”
+
+    Args:
+        filename: serialized file to load from
+
+    Return:
+        a deserialized output
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        return (json.load(f))
