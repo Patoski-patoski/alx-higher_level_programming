50a20e2b06780b9ca804baaf9a369d4d6f7c3f05
patrick okafor <patrickpeters911@gmail.com>
2023-12-06 17:04:39 -0800

    add print and str features

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""The 11-square module"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """a derived class Square that inherits Rectangle class"""
+
+    def __init__(self, size):
+        super().__init__(width=size, height=size)
+
+    def __str__(self):
+        return f"[square] {self._Rectangle__width}/{self._Rectangle__height}"
+
+    def area(self):
+        return self._Rectangle__width * self._Rectangle__height
