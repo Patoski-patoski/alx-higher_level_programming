a11781fb4c1149421060202065e301fbe92c7c03
patrick okafor <patrickpeters911@gmail.com>
2023-12-14 15:45:12 -0800

    add __strr__: a class Square that inherits from Rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,3 +18,8 @@ class Square(Rectangle):
             Nothing
     """
         super().__init__(size, size, x, y, id)
+
+    def __str__ (self):
+        """Test to  overriding the __str__ method"""
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - "
+                f"{self.width}/{self.height}")
