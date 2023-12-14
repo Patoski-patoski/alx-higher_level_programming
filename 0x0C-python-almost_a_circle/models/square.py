4b2e2dbd10c395b9ab38b1de1265fe49f0181e84
patrick okafor <patrickpeters911@gmail.com>
2023-12-14 15:40:58 -0800

    add a class Square that inherits from Rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,15 +7,14 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """The Square class"""
-    "Initialization method
-    Args:
-        size (int): the size of square
-        x (int): the vertical axis
-        y (int): the horizontal axis
-        id (int): optional
-    Return:
-        Nothing
-     """
-        self.width = size
-        self.height = size
+    def __init__ (self, size, x = 0, y = 0, id = None): 
+        """Initialization method
+        Args:
+            size (int): the size of square
+            x (int): the vertical axis
+            y (int): the horizontal axis
+            id (int): optional
+        Return:
+            Nothing
+    """
         super().__init__(size, size, x, y, id)
