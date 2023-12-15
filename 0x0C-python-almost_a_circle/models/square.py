8be4870791b3b256df6d68d27d11ff8e177628f7
patrick okafor <patrickpeters911@gmail.com>
2023-12-15 06:22:34 -0800

    add __strr__: a class Square that inherits from Rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,17 +23,6 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
-    def validate_size(self):
-        """Validate that size is equal to width and height"""
-        if self.width != self.height:
-            raise ValueError("Size must be equal to both width and \
-                             height in a Square")
-
-    def update(self, *args, **kwargs):
-        """Update attributes with validation"""
-        super().update(*args, **kwargs)
-        self.validate_size()
-
     def __str__(self):
         """Test to  overriding the __str__ method"""
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
