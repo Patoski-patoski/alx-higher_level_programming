8b2e1e18c694a460e9b14d975d90142a20e238b8
patrick okafor <patrickpeters911@gmail.com>
2023-12-15 16:43:23 -0800

    t returns the JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,10 +1,19 @@
-import unittest
+#!/usr/bin/python3
+
+""" The base module"""
+
 
 from models.base import Base
-
+from models.rectangle import Rectangle
+from models.square import Square
+import unittest
 
 class TestBase(unittest.TestCase):
     """Test cases for Rectangle class"""
+
+    def setUp(self):
+        """Set up default definition"""
+        Base._Base__nb_objects = 0
 
     def test_increment(self):
         """Test if id is incremented correctly."""
@@ -28,3 +37,11 @@ class TestBase(unittest.TestCase):
         """Test if id is of type int"""
         b1 = Base()
         self.assertIsInstance(b1.id, int)
+
+    def test_json(self):
+        """Test returns the JSON string representation of list_dictionaries"""
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        expected_outcome = '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}]'
+        self.assertEqual(json_dictionary, expected_outcome)
