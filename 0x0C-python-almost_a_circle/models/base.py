332c25547ccae5d92087d11250e4feea322c65e7
patrick okafor <patrickpeters911@gmail.com>
2023-12-15 16:34:01 -0800

    returns the JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,6 +26,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """To return the JSON string representation of list_dictionaries"""
-        if list_dictionaries is not None or list_dictionaries is not []:
+        if list_dictionaries is not None or list_dictionaries is []:
             return json.dumps(list_dictionaries)
         return []
