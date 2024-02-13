0a3827714db5cd8c2fbc46e05168b053187e11ae
patrick okafor <patrickpeters911@gmail.com>
2024-02-13 06:29:23 -0800

    a script that prints 3 lines: (like 1-multi_languages.js)

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-// a script that prints 3 lines: (like 1-multi_languages.js) but by using an 
+// a script that prints 3 lines: (like 1-multi_languages.js) but by using an
 // array of string and a loop
 
 const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
