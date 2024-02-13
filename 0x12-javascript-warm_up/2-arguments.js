cdd04283238d2737f3be7cf391ebc7565e101151
patrick okafor <patrickpeters911@gmail.com>
2024-02-12 23:04:36 -0800

    a script that prints a message depending of the number of arguments passed:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,8 +5,3 @@ if (process.argv[2] === undefined) {
 } else if (process.argv[3] === undefined) {
   console.log('Argument found');
 } else console.log('Arguments found');
-
-//console.log (process.argv[2]);
-for (var i = 0; i < process.argv.length; i++) {
-	console.log (`length ${i}: ${process.argv[i]}`);
-}
