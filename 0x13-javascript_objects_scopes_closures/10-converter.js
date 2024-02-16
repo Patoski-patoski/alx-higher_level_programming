7a17fcb9ef9d7cf7a5438d053b8f3c12394fc7f7
patrick okafor <patrickpeters911@gmail.com>
2024-02-16 07:45:50 -0800

    a function that converts a number from base 10 to another base passed as argument:

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/**
+ * a function that converts a number from base 10 to another base passed as
+ * argument
+ *
+ * Run ./10-main.js
+ **/
+
+exports.converter = function (base) {
+  return function (number) {
+    return number.toString(base)
+  }
+}
