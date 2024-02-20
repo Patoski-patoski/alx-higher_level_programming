f149aa7dd79c03cabad0fb74481be15be4532066
patrick okafor <patrickpeters911@gmail.com>
2024-02-20 07:32:11 -0800

    a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 
-SELECT tv_genres.name, tv_shows.id
+SELECT tv_genres.name AS genre, tv_shows.id
 FROM tv_genres
 LEFT JOIN tv_shows ON tv_shows.id = tv_genres.id
 ORDER BY tv_shows.id DESC;
