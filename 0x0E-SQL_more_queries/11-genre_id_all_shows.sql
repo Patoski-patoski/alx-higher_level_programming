c6970e923689a775cc1a4ac804b856c8f36ae1cb
patrick okafor <patrickpeters911@gmail.com>
2024-02-20 04:17:13 -0800

    a script that lists all shows contained in the database hbtn_0d_tvshows:  Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 --  a script that lists all shows contained in the database hbtn_0d_tvshows
 
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.show_id
 FROM tv_shows
-    INNER JOIN tv_show_genres ON tv_shows.id  = tv_show_genres.genre_id
+    INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id or NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
