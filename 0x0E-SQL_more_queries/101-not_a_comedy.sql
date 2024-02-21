d1bba373e91732bd8131755ef13e8a8a5f6f5a32
patrick okafor <patrickpeters911@gmail.com>
2024-02-21 08:33:21 -0800

    a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,7 @@
 -- a script that lists all shows without the genre Comedy in the database
 -- hbtn_0d_tvshows.
 
-SELECT DISTINCT tv_shows.title
+SELECT tv_shows.title
 FROM tv_shows
     LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
 WHERE
