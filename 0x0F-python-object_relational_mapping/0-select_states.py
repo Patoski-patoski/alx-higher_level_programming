c0c825fd50703263bbd2feb00007f9f11f430c13
patrick okafor <patrickpeters911@gmail.com>
2024-02-22 19:02:53 -0800

    a script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa
-Use (source maintest/0-select_states.sql) to load into the MySQL Server
 Execute by (./0-select_states.py username password database_name)
 """
 if __name__ == "__main__":
@@ -15,7 +14,7 @@ if __name__ == "__main__":
         port=3306
     )
     my_cursor = my_connect.cursor()
-    my_cursor.execute('SELECT * FROM states ORDER BY id ASC')
+    my_cursor.execute('SELECT * FROM states ORDER BY states.id')
     rows = my_cursor.fetchall()
     for row in rows:
         print(row)
