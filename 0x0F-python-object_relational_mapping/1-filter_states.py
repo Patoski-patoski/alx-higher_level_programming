97341eee6c99d05ba8a0ea936904c49c91b86156
patrick okafor <patrickpeters911@gmail.com>
2024-02-22 19:22:05 -0800

    a script that lists all states with a name starting with N (upper N)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-"""a script that lists all states with a name starting with N (upper N) from the
-database hbtn_0e_0_usa """
+"""a script that lists all states with a name starting with N (upper N) from
+the database hbtn_0e_0_usa """
 
 if __name__ == "__main__":
     import MySQLdb
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     rows = my_cursor.fetchall()
     for row in rows:
         print(row)
-    
+
     my_cursor.close()
     my_connect.close()
