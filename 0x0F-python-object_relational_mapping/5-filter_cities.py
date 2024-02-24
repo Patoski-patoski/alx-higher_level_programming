2e989ea685144f13418a870323a6a57dda4ac809
patrick okafor <patrickpeters911@gmail.com>
2024-02-24 11:23:25 -0800

    a script that takes in the name of a state as an argument and lists all cities of that state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-""" a script that takes in the name of a state as an argument and lists all
-    cities of that state, using the database hbtn_0e_4_usa
+""" a script that takes in the name of a state as an argument and
+    lists all cities of that state, using the database hbtn_0e_4_usa
 """
+
+from ast import arg
+
 
 if __name__ == "__main__":
     import MySQLdb
@@ -19,22 +22,24 @@ if __name__ == "__main__":
     state_name = argv[4]
 
     my_cursor = my_connect.cursor()
-query = f"""SELECT cities.name
-FROM cities
-JOIN states ON states.id = cities.state_id
-WHERE states.name = '{state_name}'
-ORDER BY cities.id;"""
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states ON states.id = cities.state_id
+    WHERE states.name = %s
+    ORDER BY cities.id;
+    """
 
-my_cursor.execute(query)
-array = []
+    my_cursor.execute(query, (state_name,))
+    array = []
 
-rows = my_cursor.fetchall()
-for row in rows:
-    city = row[0]
-    array.append(city)
+    rows = my_cursor.fetchall()
+    for row in rows:
+        city = row[0]
+        array.append(city)
 
-output = ', '.join(array)
-print(output)
+    output = ', '.join(array)
+    print(output)
 
-my_cursor.close()
-my_connect.close()
+    my_cursor.close()
+    my_connect.close()
