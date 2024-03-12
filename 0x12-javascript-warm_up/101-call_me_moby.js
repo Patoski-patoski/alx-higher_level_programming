a2cb3a4044a7719216b2b76c815ad8435e7ca4a0
Okafor Patrick C <patrickpeters911@gmail.com>
2024-03-11 22:08:45 -0700

    a function that executes x times a function.

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -3,7 +3,7 @@
 // a function that executes x times a function
 
 exports.callMeMoby = (x, theFunction) => {
-    for (let i = 0; i < x; i++){
-        theFunction();
-    }
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 };
