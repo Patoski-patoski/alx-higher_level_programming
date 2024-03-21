286053c2e98a6481713356e9ae4e209c43efbe96
patrick okafor <patrickpeters911@gmail.com>
2024-03-21 10:14:22 -0700

    a Python file similar that contains and prints the class definition of a City and prints all City objects

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 from model_state import Base, relationship
 from sqlalchemy import Column, ForeignKey, Integer, String
 
 
 class City(Base):
+
     """ Creates a class definition: city"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-   
-   # Define relationship between State
+    # Define relationship between State
     states = relationship("State", back_populates="cities")
