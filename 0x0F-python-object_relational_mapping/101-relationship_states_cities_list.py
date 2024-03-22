acac21f5fb82a484e827e8c363ac1c0f68bdfac1
patrick okafor <patrickpeters911@gmail.com>
2024-03-22 10:48:27 -0700

    a script that lists all State objects, and corresponding City objects, contained in the database

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,39 +1,29 @@
 #!/usr/bin/python3
-"""a script that lists all State objects, and corresponding City objects,
-contained in the database hbtn_0e_101_usa"""
+"""Lists all State objects and corresponding City objects contained in the
+database"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
 
 if __name__ == "__main__":
-    from sqlalchemy.orm import sessionmaker, Session
-    from sqlalchemy import create_engine
-    from sys import argv
-    from relationship_state import Base, State
-    from relationship_city import City
+    # Establish MySQL connection
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    # Create an engine to connect to MySQl server
-
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-
-    # Create all table for engine
+    # Create all tables
     Base.metadata.create_all(engine)
 
-    # Create a session to link with database
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query to get required data
-    # Use join to get both State and City objects
-    # Use order_by to sort by states.id and cities.id
-
-    city_and_state = session.query(State, City).join(
-            City, State.cities).order_by(State.id, City.id).all()
-
-    # Display results
-    current_id = None
-    for state, city in city_and_state:
-        if current_id is not state.id:
-            print(f"{state.id}: {state.name}")
-            current_id = state.id
-        print(f"\t{city.id}: {city.name}")
+    # Query for all states and corresponding cities
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
 
     session.close()
