601e76053dfc84c3818b534eaed75536eaeb692f
patrick okafor <patrickpeters911@gmail.com>
2024-03-21 23:23:31 -0700

    a script that lists all State objects, and corresponding City objects, contained in the database

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""a script that lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa"""
+
+if __name__ == "__main__":
+    from sqlalchemy.orm import sessionmaker, Session
+    from sqlalchemy import create_engine
+    from sys import argv
+    from relationship_state import Base, State
+    from relationship_city import City
+
+    # Create an engine to connect to MySQl server
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    # Create all table for engine
+    Base.metadata.create_all(engine)
+
+    # Create a session to link with database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query to get required data
+    # Use join to get both State and City objects
+    # Use order_by to sort by states.id and cities.id
+
+    city_and_state = session.query(City, State).join(
+            City, State.cities).order_by(State.id, City.id).all()
+
+    # Display results
+    current_id = None
+    for city, state in city_and_state:
+        if current_id is not state.id:
+            print(f"{state.id}: {state.name}")
+            current_id = state.id
+        print(f"\t{city.id}: {city.name}")
+
+    session.close()
