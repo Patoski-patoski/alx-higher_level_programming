465d3f1854b121e435020e4878abccac5c035230
patrick okafor <patrickpeters911@gmail.com>
2024-03-22 12:45:59 -0700

    a script that lists all State objects, and corresponding City objects

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -9,6 +9,12 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from relationship_state import Base, State
+    from relationship_city import City
+
     # Establish MySQL connection
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
