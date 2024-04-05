613cf44e8d80c596727924b3afb98fede028fddb
patrick okafor <patrickpeters911@gmail.com>
2024-04-04 19:41:39 -0700

    a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,10 +2,6 @@
 # A Bash script that takes in a URL, sends a request to that URL, and displays
 # the size of the body of the response
 
-if [ -z "$1" ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
 
 # Send a request to the URL and get the Content-Length Header
 curl -sI  "$1" | awk '/Content-Length/ {print $2}'
