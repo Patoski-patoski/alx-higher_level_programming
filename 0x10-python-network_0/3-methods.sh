d2281e44d128cbcc0f569e97aa89251d0f8eb98f
Okafor Patrick C <patrickpeters911@gmail.com>
2024-04-05 15:47:11 -0700

    edit: a Bash script that takes in a URL and displays all HTTP methods the server will accept

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -I -X OPTIONS $1 | awk '/^Allow:/ {sub(/^Allow: /, "", $0); print}'
+curl -s -I -X OPTIONS $1 | awk -F ': ' '/Allow/ {print $NF}'
