3fdeba636f832263dc064e143849e8781ac8a1a2
Okafor Patrick C <patrickpeters911@gmail.com>
2024-04-05 17:13:22 -0700

    A Bash script that takes in a URL as an argument, sends a GET request to the URL

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-
-HEADER="X-School-User-Id: 98"
-curl -H "$HEADER" "$1"
+curl -H "X-School-User-Id: 98" "$1"
