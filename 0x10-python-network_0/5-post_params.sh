6dcffadb0d1b17dd7b3e78a85557978ae6bf370e
Okafor Patrick C <patrickpeters911@gmail.com>
2024-04-05 17:34:10 -0700

    a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a POST request to the passed URL, and displays the response
-curl -X POST -d 'email=test@gmail.com&subject=I will always be here for PLD' $1
+curl -X POST -d 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
