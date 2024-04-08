e5ed399c8a688c8792b75b6489c183b6d79f1fad
root <Okafor Patrick C>
2024-04-08 16:06:58 -0700

    a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" a Python script that fetches https://alx-intranet.hbtn.io/status """
+
+import requests
+
+req = requests.get("https://alx-intranet.hbtn.io/status")
+content = req.text
+
+print('Bad response')
+print(f"\t- type: {type(content)}")
+print(f"\t- content: {content}")
