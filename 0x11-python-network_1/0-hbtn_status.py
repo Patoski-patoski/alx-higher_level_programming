99f4a372e801070414159aa440264bf95f2bf7c9
root <Okafor Patrick C>
2024-04-07 22:33:03 -0700

    script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,8 @@
 if __name__ == "__main__":
     import urllib.request as request
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-            html = response.read()
-            print('Body response:')
-            print(f"\t- type: {type(html)}")
-            print(f"\t- content: {html}")
-            print(f"\t- utf8 content: {html.decode('utf-8')}")
+        html = response.read()
+        print('Body response:')
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
