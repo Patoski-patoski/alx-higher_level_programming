10b71f4a5b48ad43e06298aec183fe55d3255f3f
root <Okafor Patrick C>
2024-04-07 22:14:15 -0700

    a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,21 +4,19 @@
 if __name__ == "__main__":
     import urllib.request
 
-# Define the URL
-url = 'https://alx-intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
 
-# Fetch the URL using a with statement to ensure the response is properly closed
-with urllib.request.urlopen(url) as response:
-    # Read the content of the response
-    content = response.read()
-    print("Body response")
+    with urllib.request.urlopen(url) as response:
+        # Read the content of the response
+        content = response.read()
+        print("Body response")
 
-    # Print the type of the content
-    print(" - type: {}".format(type(content)))
+        # Print the type of the content
+        print(" - type: {}".format(type(content)))
 
-    # Print the content itself
-    print(" - content: {}".format(content))
+        # Print the content itself
+        print(" - content: {}".format(content))
 
-    # Decode the content to a string and print it
-    content_str = content.decode('utf-8')
-    print(" - utf8 content: {}".format(content_str))
+        # Decode the content to a string and print it
+        content_str = content.decode('utf-8')
+        print(" - utf8 content: {}".format(content_str))
