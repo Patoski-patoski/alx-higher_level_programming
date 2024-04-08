6f976b5d9786719d5f0438026d8786cec6e0ed7f
root <Okafor Patrick C>
2024-04-07 21:51:20 -0700

    a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" The 0-hbtn_status Module """
+
+if __name__ == "__main__":
+    from urllib import request
+
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        print(f"Body response:")
+        content = response.read()
+        print(f"    - type: {type(content)}")
+        print(f"    - content: {content}")
+        print(f"    - utf8 content: {content.decode('utf-8')}")
