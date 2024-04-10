6964c546cfb4c2a91e5d9daae5b2e185b83594c3
Okafor Patrick C <patrickpeters911@gmail.com>
2024-04-09 23:34:49 -0700

    a Python script that takes 2 arguments in order to solve this challenge.

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,17 +2,15 @@
 """A Python script that list 10 commits (from the most recent to oldest) of
 the repository
 """
-import sys
+from sys import argv
 import requests
 
 
 if __name__ == "__main__":
-
     url = "https://api.github.com/repos/{}/{}/commits".format(
-            sys.argv[1], sys.argv[2])
+            argv[2], argv[1])
 
     commits = requests.get(url).json()
-
     try:
         for i in range(10):
             print("{}: {}".format(
