9144e426f97cf34f0d2d45b8ac16e77256b36389
Okafor Patrick C <patrickpeters911@gmail.com>
2024-05-15 04:07:10 -0700

    a script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/**
+ * a script that display the status code of a GET request.
+ *
+ *  Return nothing
+ * */
+
+const url = process.argv[2];
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: file and second argument for link', process.argv[1]);
+  process.exit(1);
+}
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  console.log('code: ', response.statusCode);
+});
