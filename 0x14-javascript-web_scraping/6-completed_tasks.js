b72b4a9fcad359837b806c70492ae72a9a6af798
Okafor Patrick C <patrickpeters911@gmail.com>
2024-05-16 13:04:37 -0700

    a script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+/**
+ *  a script that computes the number of tasks completed by user id.
+ *
+ * Return: Nothing
+ **/
+
+const request = require('request');
+const completedTask = {};
+
+const urlpath = process.argv[2];
+
+request(urlpath, (error, response, body) => {
+  if (error) {
+	  console.log(error);
+	  return;
+  }
+
+  const tasks = JSON.parse(body);
+  const completedTask = {};
+
+  for (const task of tasks) {
+    if (task.completed) {
+      if (!completedTask[task.userId]) {
+        completedTask[task.userId] = 0;
+      }
+      completedTask[task.userId]++;
+    }
+  }
+  console.log(completedTask);
+});
